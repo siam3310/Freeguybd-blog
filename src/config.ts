cfe6ae8cf868bb9c926e284f3c209614ce24291f
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-09-07 10:18:18 +0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -19,17 +19,17 @@ export const siteConfig: SiteConfig = {
     src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
-      enable: false,         // Display the credit text of the banner image
+      enable: true,         // Display the credit text of the banner image
       text: '',              // Credit text to be displayed
       url: ''                // (Optional) URL link to the original artwork or artist's page
     }
   },
-  favicon: [    
-    {
-     src: 'https://i.ibb.co/27CdzT1/82abb981-74e0-43a1-813f-6fa32f8fe0a5-removebg-preview.png',    // Path of the favicon, relative to the /public directory
-    theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-   }
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
   ]
 }
 
